e8d55bb04f37b09ca545c30dce5fb5bcdfe362ab
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-05-17 11:34:53 +0300

    building an Anagram matcher class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        original = sorted(self.word)  # Sort letters in the original word
+        matches = []
+
+        for word in word_list:
+            if sorted(word) == original and word.lower() != self.word.lower():
+                matches.append(word)
+
+        return matches
+
